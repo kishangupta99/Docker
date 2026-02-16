960cc3762a63aa0be35b4694438de7d31b48cfb1
Kishan Kumar Gupta <kishangupta@Kishans-MacBook-Air-2.local>
2026-02-17 01:05:53 +0530

    make docker file

---
+++ dockerfile
@@ -0,0 +1,6 @@
+FROM node:latest
+
+COPY . .
+RUN npm install
+EXPOSE 4000
+CMD ["node", ""]
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json(
+        [
+            {
+                id : 1,
+                name : "kishan",
+                job : "software developer",
+                salary : 900000000
+            },
+            {
+                id : 2,
+                name : "Tanya",
+                job : "software developer",
+                salary : 300000
+            },
+            {
+                id : 3,
+                name : "khushi",
+                job : "software developer",
+                salary : 2000000
+            },
+            {
+                id : 4,
+                name : "aarchie",
+                job : "software developer",
+                salary : 1000000
+            },
+             {
+                id : 5,
+                name : "aarchie",
+                job : "software developer",
+                salary : 1000000
+            }
+        ]
+    )
+})
+
+app.listen(4000, () => {
+    console.log("App is listen on port 4000");
+})
